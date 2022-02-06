05c955af544af6ea8457770cfb666bf370fd91a9
Mark Mcpherson <mark.mcpherson1@nhs.net>
2022-02-06 14:11:03 +0000

    Skip 1 test in GitHub Actions

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,8 +1,10 @@
 test_that("check_dependent_args_works", {
   tmp_pkg <- paste0(tempdir(), "/tmppkg")
   expect_equal(
-    create_package(tmp_pkg, use_git = FALSE, use_github = TRUE,
-                   use_ci = FALSE, use_precommit = FALSE),
+    create_package(tmp_pkg,
+      use_git = FALSE, use_github = TRUE,
+      use_ci = FALSE, use_precommit = FALSE
+    ),
     FALSE
   )
   expect_equal(
@@ -11,6 +13,7 @@ test_that("check_dependent_args_works", {
   expect_equal(
     create_package(tmp_pkg, TRUE, FALSE, TRUE, FALSE), FALSE
   )
+  testthat::skip_on_ci()
   expect_equal(
     basename(create_package(tmp_pkg, TRUE, FALSE, FALSE, TRUE)), "tmppkg"
   )
