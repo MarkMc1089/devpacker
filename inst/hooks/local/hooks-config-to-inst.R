9ac9d28a164cd8838346cdf7dccfef21e63de58e
Mark Mcpherson <mark.mcpherson1@nhs.net>
2022-02-22 08:06:51 +0000

    Try roxygen-dependencies hook

--- inst/hooks/local/hooks-config-to-inst.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-# We must put this
-# file under inst/ because otherwise it cannot be accessed (leaving it on top
-# level will fail r cmd check, the same when used as hidden file in inst/)
-fs::file_copy(
-  ".pre-commit-hooks.yaml", "inst/pre-commit-hooks.yaml",
-  overwrite = TRUE
-)
--- tests/testthat/test-roxy_precommit.R
+++
@@ -1,3 +0,0 @@
-test_that("dummy test", {
-  expect_equal(2 + 2, 4)
-})
