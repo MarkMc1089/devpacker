8e0a51e150bc8b7b69272b994c5d365ed14bf459
Mark Mcpherson <mark.mcpherson1@nhs.net>
2022-04-03 11:37:35 +0100

    Add create_shiny_dashboard + move arg check to utils + redo Imports

--- R/create_shiny_dashboard.R
+++ R/create_shiny_dashboard.R
@@ -1,10 +1,10 @@
-#' Create a package
-#' @description Creates a new package at given path; the package is named as the
-#'   last folder in the path.
+#' Create a shiny dashboard
+#' @description Creates a new shiny dashboard as a  package at given path; the
+#'   package is named as the last folder in the path.
 #'
 #' @param path A path. If it exists, it is used. If it does not exist, it is
 #'   created, provided that the parent path exists.
-#' @param ... Additional arguments passed to `usethis::create_package()`
+#' @param ... Additional arguments passed to `golem::create_golem()`
 #' @param use_git Initialise and make initial commits to a local git
 #'   repository. By default is `TRUE`.
 #' @param use_github Create and push to a GitHub repository. By default takes
@@ -18,19 +18,35 @@
 #'   code coverage and use the Codecov service. By default takes value of `use_ci`.
 #' @param use_lintr Set up lintr. By default is `TRUE`.
 #' @param use_tests Set up testthat. By default is `TRUE`.
-#' @inheritParams usethis::create_package
+#' @inheritParams golem::create_golem
 #' @return The path to the created package, invisibly.
 #'
 #' @export
-create_package <- function(path, use_git = TRUE, use_github = use_git,
-                           use_ci = use_github, use_precommit = use_ci,
-                           use_coverage = use_ci, use_lintr = TRUE,
-                           use_tests = TRUE, ..., open = FALSE) {
+create_shiny_dashboard <- function(path, use_git = TRUE, use_github = use_git,
+                                   use_ci = use_github, use_precommit = use_ci,
+                                   use_coverage = use_ci, use_lintr = TRUE,
+                                   use_tests = TRUE, ..., open = FALSE) {
   if (!check_create_package_args(as.list(environment())[2:5])) {
     return(invisible(FALSE))
   }
 
-  usethis::create_package(path, ..., open = open)
+  if (dir.exists(path) && (!exists(overwrite) || !overwrite)) { # Exclude Linting
+    stop(
+      paste(
+        "Project directory already exists. \n",
+        "Set `overwrite = TRUE` to overwrite anyway.\n",
+        "Be careful this will restore a brand new golem. \n",
+        "You might be at risk of losing your work !"
+      ),
+      call. = FALSE
+    )
+  }
+
+  golem_success <- golem::create_golem(path, ..., open = open)
+  if (is.null(golem_success)) {
+    usethis::ui_warn("Unable to create golem - aborting")
+    return(invisible(NULL))
+  }
   usethis::local_project(path)
 
   usethis::use_mit_license()
@@ -110,7 +126,7 @@ create_package <- function(path, use_git = TRUE, use_github = use_git,
     if (use_coverage) usethis::use_github_action("test-coverage")
   }
 
-  if (use_git) {
+  if (use_git && any(use_github, use_ci, use_precommit, use_coverage)) {
     gert::git_add(".")
     gert::git_commit("Additional initial config")
   }
--- R/utils.R
+++ R/utils.R
@@ -13,3 +13,20 @@ check_dependent_args <- function(arg, dependent_args) {
     TRUE
   }
 }
+
+
+check_create_package_args <- function(args) {
+  # use_git dependency of use_github and use_ci
+  if (!check_dependent_args(args[1], args[2:3])) {
+    return(FALSE)
+  }
+  # use_github dependency of use_ci
+  if (!check_dependent_args(args[2], args[3])) {
+    return(FALSE)
+  }
+  # use_git dependency of use_precommit
+  if (!check_dependent_args(args[1], args[4])) {
+    return(FALSE)
+  }
+  TRUE
+}
